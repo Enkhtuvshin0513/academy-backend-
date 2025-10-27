614127dbd2ab690a02ed25b461f0930fdef8dbcc
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-10-27 19:25:30 +0800

    bodolt 2

--- blog.js
+++ blog.js
@@ -1,5 +1,7 @@
 const addBtn = document.getElementById("addBtn");
 
+const blogs = [];
+
 const addBtnFunc = () => {
   const titleElement = document.getElementById("title");
   const contentElement = document.getElementById("content");
@@ -8,7 +10,21 @@ const addBtnFunc = () => {
   const title = titleElement.value;
   const content = contentElement.value;
 
-  listElement.innerHTML = `<li><h2>${title}</h2><p>${content}</p></li>`;
+  blogs.push({
+    title,
+    content
+  });
+
+  listElement.innerHTML = "";
+
+  for (let i = 0; i < blogs.length; i++) {
+    const blog = blogs[i];
+
+    const li = document.createElement("li");
+
+    li.innerHTML = `<h3>${blog.title}</h3><p>${blog.content}</p>`;
+    listElement.appendChild(li);
+  }
 };
 
 addBtn.addEventListener("click", addBtnFunc);
