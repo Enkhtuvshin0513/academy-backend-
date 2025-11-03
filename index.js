ba67b0e2253dbe6ac07caeb47ebff8916d0b80be
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-11-03 20:24:29 +0800

    jishee

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-import { startGame } from "./print.js";
+import readline from "readline/promises";
 
-startGame();
+const ql = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const askQuetion = text => {
+  return new Promise(resolve => {
+    ql.question(text, answer => {
+      resolve(answer);
+    });
+  });
+};
+
+try {
+  const nas = await askQuetion("Nasaa oruulna uu");
+  console.log(nas);
+} catch (e) {
+  console.log(e, "123123");
+}
+
+process.exit();
