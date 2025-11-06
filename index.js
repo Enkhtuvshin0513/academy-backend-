fc1848bb0c5cfd4f2e79ebd99661e2fd52945377
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-11-06 18:56:11 +0800

    update

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-import readline from "readline/promises";
+import fs from "node:fs/promises";
 
-const ql = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+const userRawData = await fs.readFile("users.json", "utf8");
+
+const users = new Array(JSON.parse(userRawData));
+
+const username = "Bat";
+const password = "123";
+
+// for (let i = 0; i < users.length; i++) {
+//   if (users[i].name === username && users[i].password === password) {
+//     user = users[i];
+//     userIndex = i;
+//   }
+// }
+
+const user = users.find(value => {
+  return value.name === username && value.password === password;
 });
 
-const askQuetion = text => {
-  return new Promise(resolve => {
-    ql.question(text, answer => {
-      resolve(answer);
-    });
-  });
-};
-
-try {
-  const nas = await askQuetion("Nasaa oruulna uu");
-  console.log(nas);
-} catch (e) {
-  console.log(e, "123123");
+if (!user) {
+  console.log("ner eswel nuuts ug buruu bn!");
+  process.exit();
 }
 
-process.exit();
+const historyRawData = await fs.readFile("history.json", "utf8");
+
+const history = JSON.parse(historyRawData);
+
+if (!history[user.name]) {
+  history[user.name] = [];
+}
+
+history[user.name].push({ amount: 1000, action: "deposit" });
+
+const historyString = JSON.stringify(history);
+
+fs.writeFile("history.json", historyString)
+  .then(() => {
+    console.log("Amjilttai bayrtai!");
+    process.exit();
+  })
+  .catch(e => {
+    console.log(e);
+    console.log("aldaa garlaa");
+  });
