fa8cf41a87de697d1cbe746ea55bacc71fda4fbb
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-11-10 20:12:00 +0800

    deposit hiiw

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fs from "node:fs/promises";
 import inquirer from "inquirer";
+import { bankAnswer } from "./bank.js";
 
 const getUsers = async () => {
   const userRawData = await fs.readFile("users.json", "utf-8");
@@ -9,16 +10,7 @@ const getUsers = async () => {
   return users;
 };
 
-const { auth } = await inquirer.prompt([
-  {
-    type: "select",
-    name: "auth",
-    message: "Login Or Signup",
-    choices: ["Login", "Signup"]
-  }
-]);
-
-if (auth === "Login") {
+const login = async () => {
   const { username, password } = await inquirer.prompt([
     {
       type: "input",
@@ -38,5 +30,75 @@ if (auth === "Login") {
     return value.username === username && value.password === password;
   });
 
-  console.log(user);
-}
+  if (!user) {
+    console.log("username eswel password buruu bn!");
+    await auth();
+  } else {
+    return bankAnswer(users, user);
+  }
+};
+
+const signup = async () => {
+  const { username, password, passwordVerify } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "Enter your username"
+    },
+    {
+      type: "password",
+      name: "password",
+      message: "Enter your password"
+    },
+    {
+      type: "password",
+      name: "passwordVerify",
+      message: "Enter your password again"
+    }
+  ]);
+
+  if (password !== passwordVerify) {
+    console.log("Password validation failed!");
+    return signup();
+  }
+
+  const users = await getUsers();
+
+  const user = users.find(value => {
+    return value.username === username;
+  });
+
+  if (user) {
+    console.log("Username not valid");
+    return signup();
+  }
+
+  users.push({ username, password, balance: 0 });
+
+  const userData = JSON.stringify(users);
+
+  await fs.writeFile("users.json", userData, "utf-8");
+
+  console.log("Amjilttai burtguulle!");
+
+  return login();
+};
+
+const auth = async () => {
+  const { authOption } = await inquirer.prompt([
+    {
+      type: "select",
+      name: "authOption",
+      message: "Login Or Signup",
+      choices: ["Login", "Signup"]
+    }
+  ]);
+
+  if (authOption === "Login") {
+    return login();
+  } else {
+    return signup();
+  }
+};
+
+auth();
