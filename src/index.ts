efde887a25e286dff3f792681ea6f591ec106ecf
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-12-22 19:39:26 +0800

    mongodb scratch

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-const text: string = "Hello TypeScript!";
+import { MongoClient } from "mongodb";
 
-console.log(text);
+const uri =
+  "mongodb+srv://enkhtuvshinej_db_user:7aLod5Z9aBfk23pu@backend-lesson.pfxqeun.mongodb.net/sample_mflix?appName=backend-lesson";
+
+const client = new MongoClient(uri);
+
+const run = async () => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("sample_mflix").command({ ping: 1 });
+
+    const aa = await client.db("sample_mflix").collection("movies").findOne();
+    console.log(aa);
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+};
+
+run();
